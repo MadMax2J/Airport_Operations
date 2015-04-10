739c5a6ec0d0b4a8396b5a06e8d98e7270e7ebbd
MadMax2J <usscork@gmail.com>
2015-04-10 14:31:03 +0100

    Update commit

---
+++ src/com/mycit/FlightNode.java
@@ -0,0 +1,14 @@
+package com.mycit;
+
+/**A node for use in data structures that deal with Objects of type Flight
+ * Created by jbyrne on 10/04/2015.
+ */
+public class FlightNode {
+
+    private Flight flight;
+    private FlightNode nextNode;
+
+
+
+
+}
